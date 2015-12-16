2c9caf5c325766b1e850dd73721f3ae9abb841cd
Alkor4ik <sergey.opanasenko@gmail.com>
2015-12-17 00:28:11 +0300

    Form create
    Logon form create and remove with my click.
    This works...  at last

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-	var form_logon = $('<form name="logon" id="logon_form"><p>Login <input type="text" name="login"></p><p>Password <input type="text" name="password"></p><p><input type="submit" name="login_send" value="Login"></p>');
-	var accordion_elem = $('ul.accordion_line li section ul');
-	for (var i = 0 ; i < accordion_elem.length; i++ ) {
-	  if ($(accordion_elem[i]).is(':hidden')) {
-	  	
-	  }
-	  else{
-		$(accordion_elem[i]).is(':hidden')
-	  }; 
-	}; 
-	
+	form_logon = $('<form name="logon" id="logon_form"><p>Login <input type="text" name="login"></p><p>Password <input type="text" name="password"></p><p><input type="submit" name="login_send" value="Login"></p>');
+	$('section > a').on('click', function(){
+  	var sections = $('section'),
+    		parent = $(this).parent(),
+    		body = $('ul', parent);
+    // reset all
+    sections.removeClass('active');
+    $('ul', sections).hide();
+    $('form', sections).remove();
+    
+    // show section
+    body.show(300);    
+    parent.toggleClass('active');
+    $(body).append(form_logon);
+  });
 
