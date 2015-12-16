b8145952638a96dffd02a319638d4f7640008ec5
Alkor4ik <sergey.opanasenko@gmail.com>
2015-12-16 19:01:52 +0300

    Try on JQuery
    $(accordion_elem[i]).is('hidden') dose not work, but
    ul.accordion_line li section ul{
    display: none;}
    and
    $(accordion_elem[i]).is('hidden') // false

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function logon_form_create() {
-	alert("Привет! Я удалил форму =)");
-	var form_logon = document.getElementsByName(logon);
-	alert(form_logon);
-}
+	var form_logon = $('<form name="logon" id="logon_form"><p>Login <input type="text" name="login"></p><p>Password <input type="text" name="password"></p><p><input type="submit" name="login_send" value="Login"></p>');
+	var accordion_elem = $('ul.accordion_line li section ul');
+	for (var i = 0 ; i < accordion_elem.length; i++ ) {
+	  if ($(accordion_elem[i]).is(':hidden')) {
+	  	
+	  }
+	  else{
+		$(accordion_elem[i]).is(':hidden')
+	  }; 
+	}; 
+	
+
