f14a7df58a9cb91dd29f8da523e980a8db524cbd
Alkor4ik <sergey.opanasenko@gmail.com>
2015-12-24 11:58:10 +0300

    Форма с переходом на новую страницу
    Добавил в основной код и поправил

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   
   
-  form_logon = $('<div id="logon_form"><p>Login <input type="text" name="login" id="login"></p><p>Password <input type="text" name="password" id="password"></p><p><button id="go"> Login </button></p></div>');
+  //form_logon = $('<div id="logon_form"><p>Login <input type="text" name="login" id="login"></p><p>Password <input type="text" name="password" id="password"></p><p><button id="go"> Login </button></p></div>');
+  form_logon = $('<p><button id="go"> Login </button></p></div>');
 
   $('section > a').on('click', function() {
     var sections = $('section'),
@@ -49,5 +50,38 @@ $(document).ready(function() {
             );
     });
   });
+    $('#reg_submit').click(function(){
+        console.log('run main script');
+        var name = $('#name').val(),
+            phone = $('#phone').val(),
+            email = $('#email').val(),
+            massage = $('#massage').html(),
+            delivery = false,
+            fail = false;
+        if (name == ""){
+            fail = 'Вы не ввели имя'
+        }
+        else if(phone == ""){
+            fail = 'Вы не ввели телефон'
+        }
+        else if(email == ""){
+            fail = 'Вы не ввели email'
+        }
+        else if($("#trust").is(":not(:checked)")){
+            fail = 'Вы не доверяете нам?'
+        }
+        else if ($("#delivery").is(":checked")){
+            delivery = true
+        }
+        if (fail){alert(fail)}
+        else{
+            sessionStorage.setItem('name',name);
+            sessionStorage.setItem('phone',phone);
+            sessionStorage.setItem('email',email);
+            sessionStorage.setItem('massage',massage);
+            sessionStorage.setItem('delivery',delivery);
+            window.location.replace('form.html');
+        }
+    });
 
 });
