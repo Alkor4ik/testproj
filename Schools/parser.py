403bdcedac489af70cda3a8b430c2e51b7ac597f
Alkor4ik <sergey.opanasenko@gmail.com>
2016-07-13 18:09:57 +0300

    Парсер школ
    Продолжаем писать парсер для школ. Парсятся все страницы со школами, но
    только как массив строк - каждая страница со школами.
    Необходимо всю инфу запихнуть в один маасив

--- Schools/parser.py
+++ Schools/parser.py
@@ -7,13 +7,6 @@ from bs4 import BeautifulSoup
 def get_html(url):
     return requests.get(url).text
 
-
-def parse_schools(html, base):
-    soup = BeautifulSoup(html, "html.parser")
-    articles = soup.find('div', class_='search-results').find_all('h2', class_='search-results-title')
-
-    return ({'scholl_name': i.a.text, 'location': 'Moscow', 'url': str(base + i.a.get('href'))} for i in articles)
-
 def parse_info(url):
     html = get_html(url)
     soup = BeautifulSoup(html, "html.parser")
@@ -26,17 +19,51 @@ def parse_info(url):
     return address.li.text.strip()
 
 
+def parse_schools(URL, base):
+    html = get_html(URL)
+    soup = BeautifulSoup(html, "html.parser")
+    articles = soup.find('div', class_='search-results').find_all('h2', class_='search-results-title')
+    schools = []
+    for i in articles:
+        school_url = str(base + i.a.get('href'))
+        school_html = get_html(school_url)
+        school_soup = BeautifulSoup(school_html, "html.parser") 
+        address = school_soup.find('ul', class_='col-sm-6 list-unstyled')
+        address = address.find_next('ul', class_='col-sm-6 list-unstyled')
+        address = address.li.text.strip()
+        schools.append({
+            'url': school_url
+            'scholl_name': i.a.text,
+            'location': address,
+            })
+    return schools
+    #({'scholl_name': i.a.text, 'location': 'Moscow', 'url': str(base + i.a.get('href'))} for i in articles)
+
+def parser_all_school(URL, base):
+    html = get_html(URL)
+    soup = BeautifulSoup(html, "html.parser")
+    pagination = soup.find('div', class_='paginator mt-section')
+    pagination = int(pagination.find_all('a')[-1].text)
+    for i in range(0,pagination*30,30):
+         URL_page = str(URL+'?s='+str(i))
+         schools = parse_schools(URL_page, base)
+         print(schools)
+    
+
+
+
 def main():
     URL = 'http://www.ucheba.ru/for-kids/schools'
     base = 'http://www.ucheba.ru'
 
-    text = get_html(URL)
-    schools = parse_schools(text, base)
+    #text = get_html(URL)
+    #schools = parse_schools(URL, base)
+    parser_all_school(URL, base)
     
     # any(print(s) for s in schools)
-    for i in schools:
+    #for i in schools:
         # print(i['url'])
-        print(parse_info(i['url']))
+    #    print (i)
 
 
 
